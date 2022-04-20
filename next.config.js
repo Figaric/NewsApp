4e590bcbda227ae62f76b8d143d7e5a41616aad4
Jigen <59481330+Figaric@users.noreply.github.com>
2022-04-20 14:58:26 +0300

    Init webapp
    Add nextjs, tailwindcss, typescript

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("next").NextConfig}
+ */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "static01.nyt.com"
+        ]
+    }
+};
+
+module.exports = nextConfig;
