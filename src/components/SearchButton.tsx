9fe45cbe591765557d7bbdbf53a68ed42e2e9c25
Jigen <59481330+Figaric@users.noreply.github.com>
2022-04-28 23:20:21 +0300

    Request the end point

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,14 +1,24 @@
 import axios from "axios";
 import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { setNewsItems } from "../redux/newsItemsSlice";
 
 function SearchButton() {
 
+    const dispatch = useDispatch();
+
     function handleClick() {
-        
+        axios.get("https://newsapi.org/v2/everything?q=charged&page=1&pageSize=15&apiKey=d3786a3a626f4cf8a5edb668f8ab757d")
+            .then(res => {
+                const newsItems = res.data.articles;
+
+                dispatch(setNewsItems(newsItems));
+            });
     }
 
     return (
         <motion.button 
+            onClick={() => handleClick()}
             whileTap={{
                 borderColor: "#f97316",
                 color: "#f97316",
@@ -22,8 +32,8 @@ function SearchButton() {
                 bg-zinc-800
                 px-5
         ">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
         </motion.button>
     );
